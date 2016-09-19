411a044e92f416c1d71b91a2f2a423243a5973ef
paceyxio <1833097998@qq.com>
2016-09-20 00:33:03 +0800

    test

--- test1/src/test1.java
+++ test1/src/test1.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
+
 /**
  * Created by xiong on 2016/9/15.
  */
 public class test1
 {
-
+    public static void main(String[] args)
+    {
+        int N,M;
+        int[] P = new int[50];
+        int[] Q = new int[50];
+        Scanner scanner = new Scanner(System.in);
+    }
 }
